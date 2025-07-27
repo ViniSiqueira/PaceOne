c16c64c52eba79831103e44e836f799e37e63357
ViniSiqueira <vinicius_siqueirabravo@hotmail.com>
2025-07-26 21:05:25 -0300

    Adição de frase de impacto

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
-  return <h2>Bem-vindo ao sistema!</h2>;
+  return (
+    <div className='container-header-home'>
+      <h2 className='text-welcome-home'>Bem-vindo ao sistema!</h2>
+      <h2 className="text-impact-phrase-home">
+        <div>
+          <span className="text-impact-phrase-bold-home">Seu primeiro</span>
+        </div>
+        <div>
+          <span className="text-impact-phrase-bold-home">passo</span> <span className="text-impact-phrase-highlight-home">muda</span>
+        </div>
+        <div>
+          <span className="text-impact-phrase-highlight-home">seu</span> <span className="text-impact-phrase-bold-home">destino!</span>
+        </div>
+      </h2>
+
+    </div>
+  );
 };
 
 export default Home;
