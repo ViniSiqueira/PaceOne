fd199abac7f1164146a537c5efa6c31ef339c6b2
ViniSiqueira <vinicius_siqueirabravo@hotmail.com>
2025-07-26 21:14:24 -0300

    Adição de cards na tela principal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,20 +3,28 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div className='container-header-home'>
-      <h2 className='text-welcome-home'>Bem-vindo ao sistema!</h2>
-      <h2 className="text-impact-phrase-home">
-        <div>
-          <span className="text-impact-phrase-bold-home">Seu primeiro</span>
-        </div>
-        <div>
-          <span className="text-impact-phrase-bold-home">passo</span> <span className="text-impact-phrase-highlight-home">muda</span>
-        </div>
-        <div>
-          <span className="text-impact-phrase-highlight-home">seu</span> <span className="text-impact-phrase-bold-home">destino!</span>
-        </div>
-      </h2>
-
+    <div>
+      <div className='container-header-home'>
+        <h2 className='text-welcome-home'>Bem-vindo ao sistema!</h2>
+        <h2 className="text-impact-phrase-home">
+          <div>
+            <span className="text-impact-phrase-bold-home">Seu primeiro</span>
+          </div>
+          <div>
+            <span className="text-impact-phrase-bold-home">passo</span> <span className="text-impact-phrase-highlight-home">muda</span>
+          </div>
+          <div>
+            <span className="text-impact-phrase-highlight-home">seu</span> <span className="text-impact-phrase-bold-home">destino!</span>
+          </div>
+        </h2>
+      </div>
+      <div className="dashboard-grid-home">
+        <div className="card-dashboard-home">Atletas ativos</div>
+        <div className="card-dashboard-home">Preparação de treinos</div>
+        <div className="card-dashboard-home card-treinos-home">Treinos da semana</div>
+        <div className="card-dashboard-home">Mensalidades</div>
+        <div className="card-dashboard-home">Eventos</div>
+      </div>
     </div>
   );
 };
